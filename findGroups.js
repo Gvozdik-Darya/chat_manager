96f0450b9aeb05477c09f0c2a9bf0925ec00f786
Daria Hvozdik <gvozdik2014@gmail.com>
2024-08-02 14:23:58 +0300

    add findGroups

---
+++ findGroups.js
@@ -0,0 +1,37 @@
+const input = require("input");
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+require("dotenv").config();
+
+const api_id = process.env.API_ID;
+const api_hash = process.env.API_HASH;
+const phone = process.env.MY_NUMBER;
+const session = process.env.SESSION;
+
+const stringSession = new StringSession(session);
+(async () => {
+  const client = new TelegramClient(stringSession, api_id, api_hash, {
+    connectionRetries: 5,
+  });
+  await client.start({
+    phoneNumber: async () => phone,
+    password: async () => await input.text("Введите пароль: "),
+    phoneCode: async () =>
+      await input.text("Введите код, полученный в Telegram: "),
+    onError: (err) => console.log(err),
+  });
+
+  //   console.log("Вы успешно вошли в систему.");
+
+  const result = await client.getDialogs({
+    archived: false,
+    isGroup: true,
+  });
+  const groupTitles = result
+    .map((chat) => chat.title)
+    .filter((el) => el.includes("Поток"));
+
+  console.log(groupTitles);
+
+  //   await client.disconnect();
+})();
